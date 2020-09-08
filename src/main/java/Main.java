84490b475d4a6478e08b9169f64b5c6aa9a90a22
haxul <s.s.starodubov@yandex.ru>
2020-09-08 21:40:52 +0400

    remove main

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class Main {
-    public static void main(String[] args) {
-        SqlParser sqlParser =  SqlParser.parse(" select * from users left join  role on role.name = users.name order by    users.id, users.name   limit 1;");
-        List<String> whereClauses = sqlParser.getSortColumns();
-    }
-}
